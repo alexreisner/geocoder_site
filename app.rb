1aac9ac73b7fc30a9796ec49d495f6d8492dc858
Alex Reisner <alex@alexreisner.com>
2011-03-18 15:56:54 -0400

    Add 'See it in action' section w/ AJAX lookup.

--- app.rb
+++ app.rb
@@ -6,6 +6,11 @@ get '/' do
   erb :home
 end
 
+get '/locations/search' do
+  @result = Geocoder.search(params[:query])
+  erb :_location, :layout => false
+end
+
 not_found do
   '404 Not Found'
 end
@@ -18,5 +23,9 @@ helpers do
 
   def separator
     '<div class="separator"></div>'
+  end
+
+  def to_json(hash)
+    hash.collect{ |p| p.map{ |i| i.inspect }.join(":") }.sort.join(",")
   end
 end
